b01f6d5d306015f468f660fd75d2eb2fe06d4f3c
Jackson <jackson.santos@conecthus.org.br>
2025-02-09 21:55:04 -0400

    test: added to model card component

---
+++ test/ui/widgets/motel_card_test.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:guia_de_moteis/core/models/motel.dart';
+import 'package:guia_de_moteis/core/models/suite.dart';
+import 'package:guia_de_moteis/ui/widgets/motel_card.dart';
+import 'package:guia_de_moteis/ui/widgets/tertiary_button.dart';
+
+void main() {
+  group("MotelCard test", () {
+    final data = Motel(
+        fantasia: "test",
+        logo:
+            "https://cdn.guiademoteis.com.br/imagens/suites/big/3148_big_9827_1.jpg",
+        bairro: "bairro",
+        distancia: 12,
+        qtdFavoritos: 1,
+        qtdAvaliacoes: 1,
+        media: 1,
+        suites: [
+          Suite(nome: "suite", qtd: 1, exibirQtdDisponiveis: false, fotos: [
+            "https://cdn.guiademoteis.com.br/imagens/suites/big/3148_big_9827_1.jpg"
+          ], itens: [], categoriaItens: [], periodos: [])
+        ]);
+    testWidgets('render element', (WidgetTester tester) async {
+      final widget = MotelCard(
+        data: data,
+      );
+
+      await tester.pumpWidget(MaterialApp(
+        home: Scaffold(
+          body: SingleChildScrollView(
+              child: Column(
+            children: [
+              widget,
+            ],
+          )),
+        ),
+      ));
+
+      expect(find.byType(MotelCard), findsOneWidget);
+    });
+
+    testWidgets('render properties element', (WidgetTester tester) async {
+      final widget = MotelCard(
+        data: data,
+      );
+
+      await tester.pumpWidget(MaterialApp(
+        home: Scaffold(
+          body: SingleChildScrollView(
+              child: Column(
+            children: [
+              widget,
+            ],
+          )),
+        ),
+      ));
+
+      final MotelCard finded = tester.widget<MotelCard>(find.byType(MotelCard));
+
+      expect(finded.data.bairro, data.bairro);
+      expect(finded.data.fantasia, data.fantasia);
+
+      expect(finded.data.suites.length, data.suites.length);
+    });
+  });
+}
