52fdddca0e5f9f828519118c93cab6dadcf5b618
jackson <msantosjck@gmail.com>
2025-02-10 13:47:41 -0400

    feat: added new colors

--- lib/theme/colors.dart
+++ lib/theme/colors.dart
@@ -3,4 +3,8 @@ import 'package:flutter/material.dart';
 class ThemeColors {
   static const purple = Color(0xFFA91E64);
   static const yellow1 = Color.fromARGB(255, 255, 187, 0);
+
+  static const header = Color(0xFFD0011B);
+
+  static const background = Color.fromARGB(255, 246, 234, 234);
 }
