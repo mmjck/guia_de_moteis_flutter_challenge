a88ec2800c55eaea4ba17aa8d5607e0310050068
jackson <msantosjck@gmail.com>
2025-02-10 13:49:23 -0400

    fix: colors use

--- lib/ui/view/home/home_screen.dart
+++ lib/ui/view/home/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:guia_de_moteis/providers/motels_provider.dart';
+import 'package:guia_de_moteis/theme/colors.dart';
 import 'package:guia_de_moteis/ui/widgets/app_bar.dart';
 import 'package:guia_de_moteis/ui/widgets/motel_card.dart';
 import 'package:provider/provider.dart';
@@ -13,26 +14,18 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   @override
-  void initState() {
-    super.initState();
-
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      Provider.of<MotelsProvider>(context, listen: false).fetchData();
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: const PreferredSize(
-            preferredSize: Size.fromHeight(70.0), // Set your custom height here
-            child: HeaderAppBar()),
-        backgroundColor: const Color.fromARGB(255, 246, 234, 234),
+          preferredSize: Size.fromHeight(70.0),
+          child: HeaderAppBar(),
+        ),
+        backgroundColor: ThemeColors.background,
         body: Container(
           child: (Provider.of<MotelsProvider>(context).isLoading)
               ? const Center(
                   child: CircularProgressIndicator(
-                    color: Color(0xFFD0011B),
+                    color: ThemeColors.header,
                   ),
                 )
               : SingleChildScrollView(
@@ -42,6 +35,15 @@ class _HomeScreenState extends State<HomeScreen> {
                   ),
                 ),
         ));
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      Provider.of<MotelsProvider>(context, listen: false).fetchData();
+    });
   }
 
   Widget _buildItems() {
