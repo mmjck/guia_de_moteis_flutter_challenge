09aa47b284275dd751fed7b73a73ff7dbccf3de7
Jackson <jackson.santos@conecthus.org.br>
2025-02-09 21:39:55 -0400

    feat: added custom typography component

---
+++ lib/ui/widgets/custom_typography.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+enum TypographyVariant { h1, h2, h3, h4 }
+
+class CustomTypography extends StatelessWidget {
+  final String text;
+  final TypographyVariant variant;
+  final TextAlign? align;
+  final TextDecoration? decoration;
+  final FontWeight? weight;
+  final Color? color;
+
+  const CustomTypography({
+    super.key,
+    required this.text,
+    required this.variant,
+    this.weight,
+    this.color,
+    this.align,
+    this.decoration,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: _buildVariant(),
+      textAlign: align,
+    );
+  }
+
+  TextStyle _buildVariant() {
+    switch (variant) {
+      case TypographyVariant.h1:
+        return TextStyle(
+          fontSize: 24,
+          fontWeight: weight,
+          color: color,
+          decoration: decoration,
+        );
+      case TypographyVariant.h2:
+        return TextStyle(
+          fontSize: 18,
+          fontWeight: weight,
+          color: color,
+          decoration: decoration,
+        );
+      case TypographyVariant.h3:
+        return TextStyle(
+          fontSize: 16,
+          fontWeight: weight,
+          decoration: decoration,
+          color: color,
+        );
+      case TypographyVariant.h4:
+        return TextStyle(
+          fontSize: 14,
+          fontWeight: weight,
+          color: color,
+          decoration: decoration,
+        );
+      default:
+        return TextStyle(
+          fontSize: 12,
+          fontWeight: weight,
+          color: color,
+          decoration: decoration,
+        );
+    }
+  }
+}
